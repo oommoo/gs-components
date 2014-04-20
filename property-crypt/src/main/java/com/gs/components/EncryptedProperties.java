8a4756efc29a511667e3a9a17c58c29b0b9f911f
Vic Bancroft <vic.bancroft@greenskycredit.com>
2014-04-20 18:37:03 -0400

    updates to use commons cipher

--- property-crypt/src/main/java/com/gs/components/EncryptedProperties.java
+++ property-crypt/src/main/java/com/gs/components/EncryptedProperties.java
@@ -6,21 +6,27 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEParameterSpec;
+import org.apache.commons.codec.binary.Base64;
+
 
 public class EncryptedProperties extends Properties {
+	public static final String DEFAULT_SECRET_KEY_FACTORY_NAME = "PBEWithMD5AndDES";
+	public static final String DEFAULT_CIPHER_NAME = "PBEWithMD5AndDES/CBC/PKCS5Padding";
+	
 	private static final long serialVersionUID = 6899133685672714938L;
-	private static sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
-	private static sun.misc.BASE64Encoder encoder = new sun.misc.BASE64Encoder();
+	private String secretKeyFactoryName = DEFAULT_SECRET_KEY_FACTORY_NAME;
+	private String cipherName = DEFAULT_CIPHER_NAME;
 	private Cipher encrypter, decrypter;
+	
 	// TODO : make configurable
-	private static byte[] salt = { (byte) 0x02, 0x03, 0x05, 0x07, 0x11, 0x13, 0x17, 0x19, 0x23 };
+	private static byte[] salt = { (byte) 0x02, 0x03, 0x05, 0x07, 0x11, 0x13, 0x17, 0x19 };
 
 	public EncryptedProperties(String password) throws Exception {
 		PBEParameterSpec ps = new javax.crypto.spec.PBEParameterSpec(salt, 20);
-		SecretKeyFactory kf = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+		SecretKeyFactory kf = SecretKeyFactory.getInstance(secretKeyFactoryName);
 		SecretKey k = kf.generateSecret(new javax.crypto.spec.PBEKeySpec(password.toCharArray()));
-		encrypter = Cipher.getInstance("PBEWithMD5AndDES/CBC/PKCS5Padding");
-		decrypter = Cipher.getInstance("PBEWithMD5AndDES/CBC/PKCS5Padding");
+		encrypter = Cipher.getInstance(cipherName);
+		decrypter = Cipher.getInstance(cipherName);
 		encrypter.init(Cipher.ENCRYPT_MODE, k, ps);
 		decrypter.init(Cipher.DECRYPT_MODE, k, ps);
 	}
@@ -42,7 +48,7 @@ public class EncryptedProperties extends Properties {
 	}
 
 	private synchronized String decrypt(String str) throws Exception {
-		byte[] dec = decoder.decodeBuffer(str);
+		byte[] dec = Base64.decodeBase64(str);
 		byte[] utf8 = decrypter.doFinal(dec);
 		return new String(utf8, "UTF-8");
 	}
@@ -50,6 +56,24 @@ public class EncryptedProperties extends Properties {
 	private synchronized String encrypt(String str) throws Exception {
 		byte[] utf8 = str.getBytes("UTF-8");
 		byte[] enc = encrypter.doFinal(utf8);
-		return encoder.encode(enc);
+		return new String(Base64.encodeBase64String(enc));
 	}
+	
+	public String getSecretKeyFactoryName() {
+		return secretKeyFactoryName;
+	}
+
+	public void setSecretKeyFactoryName(String secretKeyFactoryName) {
+		this.secretKeyFactoryName = secretKeyFactoryName;
+	}
+
+	public String getCipherName() {
+		return cipherName;
+	}
+
+	public void setCipherName(String cipherName) {
+		this.cipherName = cipherName;
+	}
+
+
 }
